2a33f0640fea5b17c602b1690e50708fd1d77942
Eric Theuri <tericmail001@gmail.com>
2025-12-05 01:16:36 +0300

    Scraper class modification

--- genai_service/utils/scrapers.py
+++ genai_service/utils/scrapers.py
@@ -11,18 +11,19 @@ try:
     logger.info("Apify Client initialized successfully.")
 except Exception as e:
     logger.exception("Failed to initialize Apify Client: %s", e)
+    client = None
 
 try:
     logger.info("Setting up Instagram scraper actor.")
     instagram_actor = client.actor("apify/instagram-scraper")
 except Exception as e:
-    logger.exception("Failed to set up Instagram scraper actor", e)
+    logger.exception("Failed to set up Instagram scraper actor %s", e)
 
 try:
     logger.info("Starting X scraper")
     # Where we have the logic for X scraper
 except Exception as e:
-    logger.exception("Error while running X scraper", e)
+    logger.exception("Error while running X scraper %s", e)
 
 try:
     logger.info("Starting Facebook scraper")
@@ -39,37 +40,38 @@ except Exception as e:
 def instagram_input(link):
         logger.info("Preparing input for Instagram scraper.")
         run_input = { 
-        "directUrls": [link],
-        "resultsType": "posts",
-        "resultsLimit": 200,
-        "searchType": "hashtag",
-        "searchLimit": 1, 
+            "directUrls": [link],
+            "resultsType": "posts",
+            "resultsLimit": 200,
+            "searchType": "hashtag",
+            "searchLimit": 1, 
         }
         return run_input
 
 
 class ScraperUtils:
-    # def __init__(self, link):
-    #     self.link = link
+    def __init__(self, link):
+        self.link = link
     
 
-    def get_instagram_results():
+    def get_instagram_results(self, link):
         try:
             logger.info("Starting Instagram scraper actor.")
-            raw_results = instagram_actor.call(run_input=instagram_input(link="https://www.instagram.com/reel/DRwZDbKjZaD/?utm_source=ig_web_copy_link&igsh=NTc4MTIwNjQ2YQ=="))
+            raw_results = instagram_actor.call(run_input=instagram_input(link=link))
+            # Instagram URL will be modified as needed
             logger.info("Instagram scraper actor finished successfully.")
             for item in client.dataset(raw_results["defaultDatasetId"]).iterate_items():
                 print(item)
         except Exception as e:
-            logger.exception("Error while running Instagram scraper actor", e)
+            logger.exception("Error while running Instagram scraper actor: %s", e)
 
     def x_scraper(self, link):
         pass
 
-    def facebook_scraper(link):
+    def facebook_scraper(self,link):
         pass
 
-    def linkedin_scraper(link):
+    def linkedin_scraper(self,link):
         pass
 
 
