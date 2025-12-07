8d32ce38e86e2fa1cb6b2d3c4c4f012a2b897d3f
bahati <bahatifidelis2004@gmail.com>
2025-12-07 12:32:13 +0300

    connected the forgot password frontend with backend to be able to send the password reset code via email and redirect back to login to log in with the new password

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -4,11 +4,13 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User, UserSchema } from './schemas/user.schema';
 import { AuthModule } from '../auth/auth.module';
+import { EmailModule } from '../email/email.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     forwardRef(() => AuthModule),
+    EmailModule,
   ],
   controllers: [UsersController],
   providers: [UsersService],
