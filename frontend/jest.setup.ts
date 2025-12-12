9083ce550ad369ec57ba3d2862f5be8279462b2e
bahati <bahatifidelis2004@gmail.com>
2025-12-12 17:32:14 +0300

    fixing lint error

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -4,6 +4,7 @@ import '@testing-library/jest-dom'
 // Extend Jest matchers
 // eslint-disable-next-line @typescript-eslint/no-namespace
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace jest {
     interface Matchers<R> {
       toBeInTheDocument(): R
